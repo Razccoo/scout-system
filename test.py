c401645b852dcf3891a8d9d7bc30e3b8ea7370a9
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-07-05 17:05:48 +0200

    .

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from PIL import Image
 from scipy import stats
 import textwrap
 from highlight_text import fig_text
-from scripts import schemas, utils
+import scripts.schemas as schemas
+import scripts.utils as utils
 
 font_normal = FontManager('https://raw.githubusercontent.com/googlefonts/roboto/main/'
                         'src/hinted/Roboto-Regular.ttf')
