4f14a8760ed8a44d52e5d2ce2fd8460782e2cb75
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-03 16:59:33 +0200

    .

--- pages/V2_Scouting.py
+++ pages/V2_Scouting.py
@@ -981,7 +981,7 @@ def selected_player_data(filtered_data, comparison_data, player_name, player_age
                     mean_value = combined_data[metric].mean()
                     
                     # Calculate the percentile of the mean value within the combined data
-                    mean_percentile = stats.percentileofscore(ranked_values, mean_value) / 100
+                    mean_percentile = rank_column_percentile(combined_data[metric], mean_value)
                     
                     # Append the values for plotting
                     radar_values.append(player_ranked_value)
