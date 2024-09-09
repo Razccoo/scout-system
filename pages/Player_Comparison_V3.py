4c1ad5907103f85e304e3dd234d36f8d4d76220f
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-09 19:12:00 +0200

    .

--- pages/Player_Comparison_V3.py
+++ pages/Player_Comparison_V3.py
@@ -136,7 +136,7 @@ def generate_mplsoccer_radar_chart(player_data, player_names, player_teams, metr
     fig.figimage(twitter_icon, 30, 10, zorder=3, alpha=1)  # Position the icon at the bottom left of the figure
 
     # Add your Twitter handle
-    fig.text(0.1, 0.02, '@AlfieScouting', fontsize=12, ha='right', va='center', color='#FFFFFF')
+    fig.text(0.1, 0.02, '@AlfieScouting', fontsize=12, ha='left', va='center', color='#FFFFFF')
     
     fig.set_facecolor('#070707')
     
