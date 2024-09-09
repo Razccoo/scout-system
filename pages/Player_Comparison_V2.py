0fe21372360e6f391f93dfa2e9c3f2c5868a10df
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-09 16:05:27 +0200

    .

---
+++ pages/Player_Comparison_V2.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pandas as pd
+from scripts import utils
+from scripts.config import get_position_to_schema, get_params_list, get_schema_params, get_label_mapping, get_column_mapping, position_options
+from matplotlib.font_manager import FontProperties
+
+st.title("Player Comparison Radar Chart")
+st.sidebar.header("Player Selection")
+
+schema_type = st.sidebar.toggle("Kendi şablonumu kullanmak istiyorum")
+
+all_leagues_df = utils.load_top_9_leagues()
+selected_position = st.sidebar.selectbox("Pozisyon Seçiniz", position_options+["All"])
+df = utils.filter_by_position(all_leagues_df, selected_position)
+df = df[df['Minutes played'] >= 900]
+currentseason = df[df['Season'] == '23-24']
+
+selected_players = st.sidebar.multiselect("Select Players to Compare", df['Player'].unique())
+
+seasons = {}
+for player in selected_players:
+    seasons[player] = st.sidebar.selectbox(f"Select Season for {player}", df[df['Player'] == player]['Season'].unique())
+    
+if schema_type:
+    st.sidebar.header("Özel Şablon Oluşturma")
+    custom_schema_name = st.sidebar.text_input("Özel Şablon Adı")
+    available_metrics = get_params_list()
+    
+    # Initialize session state for custom schema creation
+    if "custom_schema" not in st.session_state:
+        st.session_state.custom_schema = []
+
+    # Select metrics for the custom schema
+    st.session_state.custom_schema = st.sidebar.multiselect(
+        "Şablon için metrikleri seçin", 
+        available_metrics, 
+        st.session_state.custom_schema
+    )
+
+    # Display selected metrics for review
+    if st.session_state.custom_schema:
+        st.sidebar.write("Seçilen Metrikler:")
+        st.sidebar.write(", ".join(st.session_state.custom_schema))
+
+    # Save the custom schema
+    if st.sidebar.button("Özel Şablonu Kaydet"):
+        if custom_schema_name:
+            if "custom_schemas" not in st.session_state:
+                st.session_state.custom_schemas = {}
+            st.session_state.custom_schemas[custom_schema_name] = st.session_state.custom_schema
+            st.sidebar.success(f"Özel şablon '{custom_schema_name}' kaydedildi.", icon="✅")
+            st.session_state.custom_schema = []  # Reset custom schema after saving
+        else:
+            st.sidebar.error("Lütfen şablon adı giriniz.")
