0bd20863536dfe380c80a92cdc19580c8089da40
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-09 18:35:53 +0200

    .

--- pages/Player_Comparison_V3.py
+++ pages/Player_Comparison_V3.py
@@ -91,7 +91,7 @@ def generate_mplsoccer_radar_chart(player_data, player_names, metrics, radar_hig
         radar_poly, vertices = radar.draw_radar_solid(
             values=player_stats.tolist(),
             ax=ax,
-            kwargs={'facecolor': color, 'alpha': 0.3, 'edgecolor': color, 'lw': 2}  # Unique color for each player
+            kwargs={'facecolor': color, 'alpha': 0.5, 'edgecolor': color, 'lw': 2}  # Unique color for each player
         )
 
         # Add 'o' markers for each metric point using scatter
@@ -100,10 +100,10 @@ def generate_mplsoccer_radar_chart(player_data, player_names, metrics, radar_hig
 
         # Alternate player name placement between left and right
         if idx % 2 == 0:  # Even index - place on the left
-            ax.text(-0.1, 1.1 - (idx // 2) * 0.05, player_name, ha='right', va='center', transform=ax.transAxes,
+            ax.text(0, 1.1 - (idx // 2) * 0.05, player_name, ha='right', va='center', transform=ax.transAxes,
                     fontsize=12, weight='bold', color=color)
         else:  # Odd index - place on the right
-            ax.text(1.1, 1.1 - ((idx - 1) // 2) * 0.05, player_name, ha='left', va='center', transform=ax.transAxes,
+            ax.text(1, 1.1 - ((idx - 1) // 2) * 0.05, player_name, ha='left', va='center', transform=ax.transAxes,
                     fontsize=12, weight='bold', color=color)
 
     # Draw the parameter labels and range labels
