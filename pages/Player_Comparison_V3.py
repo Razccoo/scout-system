4a7a9e25c77faefec62778e129d47c71aa0ded04
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-10 13:19:57 +0200

    .

--- pages/Player_Comparison_V3.py
+++ pages/Player_Comparison_V3.py
@@ -165,6 +165,10 @@ def generate_mplsoccer_radar_chart(player_data, player_names, player_teams, metr
     fig.text(0.52, 0.05, 'ALFIESCOUTING', fontsize=12, ha='center', va='center', color='#FFFFFF',
             fontproperties=robotto_bold.prop)  # Use the custom font
     
+    fig.text(0.01, 0.5, 'Tüm değerler 90 başına.', fontsize=12,
+                                    fontproperties=robotto_bold.prop, ha='left',
+                                    va='center', color='#FFFFFF')
+    
     fig.set_facecolor('#070707')
     
     # Return the figure for rendering
