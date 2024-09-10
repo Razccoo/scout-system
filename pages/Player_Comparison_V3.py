09c0292b9e7c3d5988c7e980fa37c1953c321fc1
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-10 12:34:55 +0200

    .

--- pages/Player_Comparison_V3.py
+++ pages/Player_Comparison_V3.py
@@ -132,8 +132,8 @@ def generate_mplsoccer_radar_chart(player_data, player_names, player_teams, metr
                     fontsize=10, color=color)
 
     # Draw the parameter labels and range labels
-    radar.draw_param_labels(ax=ax, wrap=15, offset=1, color='#FFFFFF', fontweight='bold', fontsize=12)
-    radar.draw_range_labels(ax=ax, offset=0.1, color='#FFFFFF', fontweight='bold', fontsize=10)
+    radar.draw_param_labels(ax=ax, wrap=15, offset=1, color='#FFFFFF', fontweight=400, fontsize=12)
+    radar.draw_range_labels(ax=ax, offset=0.1, color='#FFFFFF', fontweight=500, fontsize=10)
     
     # # Title and final adjustments
     # ax.set_title("Player Comparison Radar Chart", size=20, pad=20)
