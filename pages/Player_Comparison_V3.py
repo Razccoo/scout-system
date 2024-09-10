8d29188912a36e3e7f15b2012b689bbbc84385a8
Ramazan Arslan <91985764+Razccoo@users.noreply.github.com>
2024-09-10 12:42:08 +0200

    .

--- pages/Player_Comparison_V3.py
+++ pages/Player_Comparison_V3.py
@@ -72,7 +72,7 @@ def generate_mplsoccer_radar_chart(player_data, player_names, player_teams, metr
     max_range = radar_high.tolist()
 
     # Map the selected metrics to their new labels using the label_mapping dictionary
-    mapped_labels = [get_column_mapping.get(metric, metric) for metric in metrics]  # Use original if no mapping exists
+    mapped_labels = [get_column_mapping().get(metric, metric) for metric in metrics]  # Use original if no mapping exists
 
     # Initialize the Radar object
     radar = Radar(
